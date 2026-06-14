031b5a05a5fc970e4de96f7c59ea8b874a14e270
lawrencd2553-source <lawrencd2553@student.faytechcc.edu>
2026-06-14 01:05:23 -0400

    Add files via upload

---
+++ P1LAB1_DowdyDevon.py
@@ -0,0 +1,16 @@
+# Devon Dowdy
+# 06/14/2026
+# P1LAB1
+# Pythion program that gets the user's first and last name and displays a greeting using both variables
+
+
+# Get first and last name from user
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last nameJ: ")
+# print(type(first_name))
+print()
+# Display greeting using first_name and last_name variables
+
+# Use concatenation
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
+print()
